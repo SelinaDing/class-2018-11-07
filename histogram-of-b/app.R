3be83b4a246bbb84c9ad5d2d5b77d993da076bdf
丁村鸿鹄 <dingcunhonghu@dingcunhonghudeMacBook-Air.local>
2018-11-07 17:51:49 -0500

    app

---
+++ histogram-of-b/app.R
@@ -0,0 +1,81 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+bcData <- read_rds("allfiles.rds")
+
+# Data pre-processing ----
+# Tweak the "am" variable to have nicer factor labels -- since this
+# doesn't rely on any user inputs, we can do this once at startup
+# and then use the value throughout the lifetime of the app
+#mpgData <- mtcars
+#mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+
+# Define UI for miles per gallon app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Histogram of b/c"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Selector for variable to plot against mpg ----
+      selectInput("variable", 
+                  "Variable:",
+                  c("b" = "b",
+                    "c" = "c"),
+                  selected = "b")
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Formatted text for caption ----
+      h3(textOutput("caption")),
+      
+      # Output: Plot of the requested variable against mpg ----
+      plotOutput("histogram")
+      
+    )
+  )
+)
+
+# Define server logic to plot various variables against mpg ----
+server <- function(input, output) {
+  
+  # Compute the formula text ----
+  # This is in a reactive expression since it is shared by the
+  # output$caption and output$mpgPlot functions
+  formulaText <- reactive({
+    paste("Histogram of", input$variable)
+  })
+  
+  # Return the formula text for printing as a caption ----
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg ----
+  # and only exclude outliers if requested
+  output$histogram <- renderPlot({
+    ggplot(data = bcData, aes_string(x = input$variable)) +
+      geom_histogram() 
+  })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
--- scratch.R
+++ scratch.R
@@ -10,22 +10,51 @@ library(fs)
 
 # 1. Read data/ex_926_I.csv into a tibble and provide a summary.
 
+ex926i <- read_csv("data/ex_926_I.csv")
+ex926i
+summary(ex926i)
+
 # 2. Create a vector with all the file names in data/.
 
+#filenames <- list.files("data/")
+
+filenames <- dir_ls("data/")
+
 # 3. Create a vector with just the file names that have an "A" in them.
+
+#filenames_a <- list.files("data/", regex("A"))
+
+  str_subset(filenames, "A")
 
 # 4. Read in all the files into one big tibble. Check out ?map_dfr . . .
 # Background reading here:
 # https://r4ds.had.co.nz/iteration.html#the-map-functions
 
+allfiles <- map_dfr(filenames, read_csv, .id = "source")
+
 # 5. Read in everything and also add a new variable, source, which records the
 # file name from which the data came.
 
+
 # 6. Find the 4 files with the largest number of observations.
 
+allfiles %>%
+  count(source) %>%
+  arrange(desc(n))
+  
 # 7. Write a function which takes a character string like "A" and then reads in
 # all the files which have "A" in the name.
+
+have_a <- function(x) {
+  str_subset(x, "A")
+}
+
+allfiles_a <- map_dfr(have_a(filenames), read_csv, .id = "source")
 
 # 8. Create a Shiny App which displays the histogram of b, allowing the user to
 # subset the display for specific values of c.
 
+ggplot(allfiles, aes(x = b)) +
+  geom_histogram()
+
+write_rds(allfiles, "histogram-of-b/allfiles.rds")
